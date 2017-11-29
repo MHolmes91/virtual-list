448a23927902820e58fe9ca83b8b4ece1ad7c72c
Mark Holmes <mark.holmes@calabrio.com>
2017-11-29 12:48:18 -0600

    Improved documentation of parameters. Better handling of optional values. Separated out generatorFn(). Container state stored in local object instead of DOM.

--- vlist.js
+++ vlist.js
@@ -1,7 +1,8 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (C) 2013 Sergi Mansilla
+ * Original Work Copyright (C) 2013 Sergi Mansilla
+ * Modified Work Copyright (C) 2017 Mark Holmes
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the 'Software'), to deal
@@ -27,16 +28,32 @@
 /**
  * Creates a virtually-rendered scrollable list.
  * @param {object} config
+ * @param {int} config.w - Container Width (optional)
+ * @param {int} config.h - Container Height
+ * @param {int} config.itemHeight - Height of each individual item in pixels
+ * @param {String[]|Element[]|Object[]} config.items - An array of items to be used in this list (Can be strings, DOM elements, or an object to be transformed using generatorFN) (optional)
+ * @param {Function} config.generatorFn - {(int) => {Element}} Function Input is the index of the item being generated, output is a DOMElement to be inserted into the list (optional)
+ * @param {int} config.totalRows - The total number of items (if different from the length of the items array) (optional)
+ * @param {int} config.itemBuffer - The number of items to have loaded at a time, this should be larger than the height of the container / height of each item (optional)
  * @constructor
  */
 function VirtualList(config) {
-  var width = (config && config.w + 'px') || '100%';
-  var height = (config && config.h + 'px') || '100%';
+  var width = (config && config.w) ? (config.w + 'px') : '100%';
+  var height = (config && config.h) ? (config.h + 'px') : '100%';
   var itemHeight = this.itemHeight = config.itemHeight;
 
-  this.items = config.items;
-  this.generatorFn = config.generatorFn;
-  this.totalRows = config.totalRows || (config.items && config.items.length);
+  this.items = config.items ? config.items : [];
+
+  /*
+  Will be an object containing all nodes currently in the container in the form {status: 0/1, node: DOMNode}
+  Status 0 means the node is hidden and pending deletion, 1 means it is being displayed
+  */
+  this.itemsInContainer = {};
+
+  if(config.generatorFn){
+    this.generatorFn = config.generatorFn;
+  }
+  this.totalRows = config.totalRows || this.items.length;
 
   var scroller = VirtualList.createScroller(itemHeight * this.totalRows);
   this.container = VirtualList.createContainer(width, height);
@@ -47,7 +64,7 @@ function VirtualList(config) {
   this.cachedItemsLen = config.itemBuffer ? config.itemBuffer : screenItemsLen * 3;
   this._renderChunk(this.container, 0);
 
-  var self = this;
+  var _this = this;
   var lastRepaintY;
   var maxBuffer = screenItemsLen * itemHeight;
   var lastScrolled = 0;
@@ -56,12 +73,16 @@ function VirtualList(config) {
   // the nodes that are not used anymore
   this.rmNodeInterval = setInterval(function() {
     if (Date.now() - lastScrolled > 100) {
-      var badNodes = self.container.querySelectorAll('[data-rm="1"]');
-      for (var i = 0, l = badNodes.length; i < l; i++) {
-        var removedNode = self.container.removeChild(badNodes[i]);
-        removedNode.removeAttribute('data-rm');
-        removedNode.style.display = null;
-      }
+      var badItemIndicies = Object.keys(_this.itemsInContainer).filter(function(index){
+        return _this.itemsInContainer[index].status === 0;
+      });
+
+      badItemIndicies.forEach(function(index){
+        var badItem = _this.itemsInContainer[index];
+        var removedNode = _this.container.removeChild(badItem.node);
+        removedNode.style.display = "";
+        delete _this.itemsInContainer[index];
+      });
     }
   }, 300);
 
@@ -69,7 +90,7 @@ function VirtualList(config) {
     var scrollTop = e.target.scrollTop; // Triggers reflow
     if (!lastRepaintY || Math.abs(scrollTop - lastRepaintY) > maxBuffer) {
       var first = parseInt(scrollTop / itemHeight) - screenItemsLen;
-      self._renderChunk(self.container, first < 0 ? 0 : first);
+      _this._renderChunk(_this.container, first < 0 ? 0 : first);
       lastRepaintY = scrollTop;
     }
 
@@ -80,28 +101,41 @@ function VirtualList(config) {
   this.container.addEventListener('scroll', onScroll);
 }
 
-VirtualList.prototype.createRow = function(i) {
+VirtualList.prototype.generatorFn = function(i){
   var item;
-  if (this.generatorFn)
-    item = this.generatorFn(i);
-  else if (this.items) {
-    if (typeof this.items[i] === 'string') {
-      var itemText = document.createTextNode(this.items[i]);
+  //If we have an array of items and if we have an item at the current index
+  if(this.items && this.items[i]){
+    //Create the item as a text node if it is a string
+    if(typeof this.items[i] === 'string'){
       item = document.createElement('div');
       item.style.height = this.itemHeight + 'px';
+
+      var itemText = document.createTextNode(this.items[i]);
       item.appendChild(itemText);
-    } else {
+    }
+    //Otherwise, assume the item is a DOM element and use it directly
+    else{
       item = this.items[i];
     }
   }
+  //If blank, create a blank div
+  else{
+    item = document.createElement('div');
+    item.style.height = this.itemHeight + 'px';
+  }
+
+  return item;
+};
+
+VirtualList.prototype.createRow = function(i) {
+  var item = this.generatorFn(i);
 
   item.classList.add('vrow');
-  item.setAttribute('data-vi', i);
-  item.removeAttribute('data-rm');
-
-  item.style.display = null;
+  item.style.display = '';
   item.style.position = 'absolute';
   item.style.top = (i * this.itemHeight) + 'px';
+
+  this.itemsInContainer[i] = {status: 1, node: item};
 
   return item;
 };
@@ -117,9 +151,18 @@ VirtualList.prototype.createRow = function(i) {
  * @return {void}
  */
 VirtualList.prototype._renderChunk = function(node, from) {
+  var _this = this;
   var finalItem = from + this.cachedItemsLen;
-  if (finalItem > this.totalRows)
+  if (finalItem > this.totalRows){
     finalItem = this.totalRows;
+  }
+
+  // Hide and mark obsolete nodes for deletion.
+  Object.keys(this.itemsInContainer).forEach(function(key){
+    var itemInContainer = _this.itemsInContainer[key];
+    itemInContainer.status = 0;
+    itemInContainer.node.style.display = 'none';
+  });
 
   // Append all the new rows in a document fragment that we will later append to
   // the parent node
@@ -128,11 +171,6 @@ VirtualList.prototype._renderChunk = function(node, from) {
     fragment.appendChild(this.createRow(i));
   }
 
-  // Hide and mark obsolete nodes for deletion.
-  for (var j = 1, l = node.childNodes.length; j < l; j++) {
-    node.childNodes[j].style.display = 'none';
-    node.childNodes[j].setAttribute('data-rm', '1');
-  }
   node.appendChild(fragment);
 };
 
